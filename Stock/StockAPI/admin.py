3c431e86cca56eb9ff4af9f59a1a30a952d59086
vikitripathi <abhishek09011993@gmail.com>
2014-11-08 12:55:35 +0530

    feat:Mess Stock Admin and API build

---
+++ Stock/StockAPI/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from StockAPI.models import Item,Unit,Transaction
+
+# Register your models here.
+class UnitAdmin(admin.ModelAdmin):
+	list_display = ('name','shorthand')
+
+class ItemAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Item Details',               {'fields': ['item_id','item_name','item_unit','quantity','balance']}),
+        ('Date information', {'fields': ['timestamp']}),
+    ]
+    list_display = ('item_name','item_unit', 'quantity','balance')
+
+class TransactionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Transaction Details',               {'fields': ['item','transaction_type','quantity','cost','inventory','date','consumption']}),
+        ('Date information', {'fields': ['timestamp']}),
+        ('Comments', {'fields': ['comments']}),
+    ]
+    list_display = ('item','quantity', 'inventory','timestamp')
+    list_filter=['timestamp']
+    search_fields=['item']
+
+admin.site.register(Item,ItemAdmin)
+admin.site.register(Unit,UnitAdmin)
+admin.site.register(Transaction,TransactionAdmin)
